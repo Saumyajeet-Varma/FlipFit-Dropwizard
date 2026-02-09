10501c4c1cca25432099540dd5e2d87387be740f
Saumyajeet Varma <saumyajeetv@gmail.com>
2026-02-09 16:23:13 +0530

    database connection done

--- src/main/java/com/flipfit/FlipFitApplication.java
+++ src/main/java/com/flipfit/FlipFitApplication.java
@@ -2,8 +2,10 @@ package com.flipfit;
 
 import com.flipfit.resources.HelloWorldResourse;
 import io.dropwizard.Application;
+import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.jdbi.v3.core.Jdbi;
 
 public class FlipFitApplication extends Application<FlipFitConfiguration> {
 
@@ -23,6 +25,10 @@ public class FlipFitApplication extends Application<FlipFitConfiguration> {
 
     @Override
     public void run(final FlipFitConfiguration configuration, final Environment environment) {
+
+        final JdbiFactory factory = new JdbiFactory();
+        final Jdbi jdbi = factory.build(environment, configuration.getDataSourceFactory(), "mysql");
+
         environment.jersey().register(new HelloWorldResourse());
     }
 
--- src/main/java/com/flipfit/FlipFitConfiguration.java
+++ src/main/java/com/flipfit/FlipFitConfiguration.java
@@ -1,10 +1,20 @@
 package com.flipfit;
 
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class FlipFitConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @Valid
+    @NotNull
+    @JsonProperty("database")
+    private DataSourceFactory database = new DataSourceFactory();
+
+    public DataSourceFactory getDataSourceFactory() {
+        return this.database;
+    }
 }
